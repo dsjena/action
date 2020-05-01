f75014d9f5ccad773491a300b699f323fbb2c9ba
dsjena <36220371+dsjena@users.noreply.github.com>
2020-05-02 02:58:44 +0530

    Test 5

--- run.sh
+++ run.sh
@@ -1,8 +1,5 @@
-urlpath="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series"
-CONFIRMED="time_series_covid19_confirmed_global.csv"
+pwd
+echo ${GITHUB_WORKSPACE}
+ls ${GITHUB_WORKSPACE}
 
-rm -f ${CONFIRMED} ${DEATH} ${RECOVERED} ${CONFIRMED_US} ${DEATH_US}
-mkdir -p csv
-jhu=${CONFIRMED}
-wget ${urlpath}/${jhu}
-mv ${jhu} csv/confirmed.csv
+Rscript R/readData.R
