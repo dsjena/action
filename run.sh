b51d450dca2301ae5d8046bcb6e1a7e650a918ed
dsjena <36220371+dsjena@users.noreply.github.com>
2020-05-05 22:56:53 +0530

    test 14

--- run.sh
+++ run.sh
@@ -5,3 +5,5 @@ ls ${GITHUB_WORKSPACE}
 Rscript R/readData.R
 
 Rscript -e 'rmarkdown::render("india/index.Rmd", output_format = "india/index.html")'
+Rscript -e 'rmarkdown::render("india.Rmd")'
+Rscript -e 'rmarkdown::render("world.rmd")'
